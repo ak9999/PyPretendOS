8ebe0985e62ac49664b1cebef96b7cd5fd341532
Abdullah Khan <khanabdullahj@gmail.com>
2016-03-08 10:46:00 -0500

    Added TODO.txt and program info to Run.py. Also made Run.py not executable.

--- Run.py
+++ Run.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
+"""
+Author: Abdullah Khan
+Program: Run.py
+Description: Simulates an operating system and system hardware.
+Build instructions: Make sure Run.py is executable and run it.
+"""
+
 from PretendSystem import PretendSystem as pos
 
 totally_real_system = pos()  # Construct system!
