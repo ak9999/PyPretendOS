37d502d562b46ae869681155ae284fa69547dff2
Abdullah Khan <khanabdullahj@gmail.com>
2016-03-10 13:11:50 -0500

    Cleans up __pycache__ directory on exit!

--- Run.py
+++ Run.py
@@ -9,6 +9,14 @@ Build instructions: Make sure Run.py is executable and run it.
 
 from PretendSystem import PretendSystem as pos
 
+import shutil
+
 totally_real_system = pos()  # Construct system!
 print()
 totally_real_system.printsys()
+
+# Clean up the pycache
+try:
+    shutil.rmtree("__pycache__")
+except FileNotFoundError:
+    print("Failed to remove __pycache__ directory")
