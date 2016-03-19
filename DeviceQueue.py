623b906e79d7107d4355d33756ea4b25f465a5e4
Abdullah Khan <khanabdullahj@gmail.com>
2016-03-19 18:03:18 -0400

    [FIXED] Better printing.

--- DeviceQueue.py
+++ DeviceQueue.py
@@ -10,14 +10,14 @@ from collections import deque
 This is the generic device queue that will be inherited by the actual devices.
 """
 
-class DeviceQueue(object):
 
+class DeviceQueue(object):
     def __init__(self):
         self.q = deque()
         self.number = None
         self.device_name = None
 
-    def add(self, block):  #  Add Process Control Blocks to queue.
+    def add(self, block):  # Add Process Control Blocks to queue.
         self.q.append(block)
 
     def remove(self):
@@ -39,12 +39,7 @@ class DeviceQueue(object):
     def print_device_queue(self):
         string = ""
         for block in self.q:
-            string = ("%s\t%s\t%s\t%s\t%s"
-                  % (str(self.pid).rjust(3),
-                     str(self.filename).rjust(8),
-                     str(self.memstart).rjust(8),
-                     str(self.rw).rjust(3),
-                     str(self.file_length).rjust(11)))
+            string += str(block + "\n")
         return string
 
     def __str__(self):
@@ -53,22 +48,18 @@ class DeviceQueue(object):
 
 
 class DiscQueue(DeviceQueue):
-
     def __init__(self):
         DeviceQueue.__init__(self)
-        self.add()
         self.device_name = "c%d" % str(self.get_number())
 
 
 class DiskQueue(DeviceQueue):
-
     def __init__(self):
         DeviceQueue.__init__(self)
         self.device_name = "d%d" % str(self.get_number())
 
 
 class PrinterQueue(DeviceQueue):
-
     def __init__(self):
         DeviceQueue.__init__(self)
         self.device_name = "p%d" % str(self.get_number())
