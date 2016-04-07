e1f3c085a4d9d550285e823a3c2508309ee68c64
Abdullah Khan <khanabdullahj@gmail.com>
2016-04-06 20:38:56 -0400

    Removed unnecessary imports and brought code closer to PEP8.

--- Signals.py
+++ Signals.py
@@ -5,10 +5,9 @@ Description: Handles making sure system calls and interrupts are valid.
 """
 
 import re
-import DeviceQueue
-from ReadyQueue import *
 from PretendSystem import cleanup
 from PCB import ProcessControlBlock as PCB
+
 
 def leave():
     print()
@@ -70,24 +69,23 @@ def signal(letter, system):
     use dictionaries to mimic that functionality.
     """
     switch_case = \
-    {
-        "A": arrival,  # put PCB into ready queue
-        "S": snapshot,
-        "t": terminate
-    }
+        {
+            "A": arrival,  # put PCB into ready queue
+            "S": snapshot,
+            "t": terminate
+        }
 
     try:
         if letter in switch_case:
             #  Get function from the switch_case dictionary
             function = switch_case.get(letter)
+            return function(system)  # Return the function and execute it.
         else:
-            print("Bad input.") #maybe send to device here
+            print("Bad input.")  # maybe send to device here
             return
     except TypeError:
         print("You broke it.")
         leave()
-
-    return function(system)  # Return the function and execute it.
 
 
 def send_to_device(command, system):
@@ -182,14 +180,17 @@ def valid_signal(pattern):
     """
     return re.compile(r"^[AcdrStp]{1}$").match(pattern) is not None
 
+
 def valid_device(pattern):
     """
     Return whether the pattern is a valid device
     """
     return re.compile(r"^[cdp][0-9]{1}$").match(pattern) is not None
 
+
 def valid_complete(pattern):
     return re.compile(r"^[CDP][0-9]{1}$").match(pattern) is not None
+
 
 def valid_readwrite(pattern):
     return re.compile(r"^[rw]{1}$").match(pattern) is not None
