80c79721e1a1c0b1c7b8a5511c669964103d5999
Abdullah Khan <khanabdullahj@gmail.com>
2016-04-15 13:23:25 -0400

    Added header.

--- __main__.py
+++ __main__.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Author: Abdullah Khan
+Program: __main__.py
+Description: Simulates an operating system and system hardware.
+Build instructions: None.
+"""
+
 import Run
 
 Run.main()
