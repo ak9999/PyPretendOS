148fc18755833904653bfd98ff3c53aec017ee00
Abdullah Khan <khanabdullahj@gmail.com>
2016-04-15 08:34:57 -0400

    Most things works except for printing the burst.

--- PCB.py
+++ PCB.py
@@ -21,12 +21,41 @@ available_address = 256
 class ProcessControlBlock:
     def __init__(self):
         self.pid = None
-        self.memstart = None
-        self.memend = None
+        self.memstart = 0
+        self.memend = 0
         self.rw = ''
         self.filename = None
-        self.file_length = None
+        self.file_length = 0
+        self.location = 0  # This is the cylinder the data is on if PCB is going to disk.
+        self.cpu_total = 0
+        self.avg_burst = 0
+        self.bursts = list()
         create_block(self)
+
+    def time_cpu(self):
+        print("How long has the current process been in the CPU?:", end=' ')
+        try:
+            self.cpu_total = float(input())
+            if self.cpu_total < 0:
+                print("Negative numbers not possible. Try again.")
+                self.time_cpu()
+        except ValueError:
+            print("Error, try again.")
+            self.time_cpu()
+        except KeyboardInterrupt:
+            print()
+            cleanup()
+            exit()
+        except EOFError:
+            print()
+            cleanup()
+            exit()
+
+    def add_time(self, time):
+        self.cpu_total += time
+
+    def get_total_cpu_time(self):
+        return self.cpu_total
 
     def get_pid(self):
         return self.pid
@@ -47,7 +76,10 @@ class ProcessControlBlock:
         return self.rw
 
     def get_mem_block(self):
-        print(str(self.memstart) + "," + str(self.memend))
+        print(str(self.memstart) + ',' + str(self.memend))
+
+    def get_cylinder(self):
+        return int(self.location)
 
     def set_memstart(self):
         global available_address
@@ -67,7 +99,7 @@ class ProcessControlBlock:
         self.rw = operation
 
     def set_file_length(self):
-        print("File length:", end=" ")
+        print("File length:", end=' ')
         try:
             self.file_length = int(input())
         except ValueError:
@@ -83,7 +115,7 @@ class ProcessControlBlock:
             exit()
 
     def set_file_name(self):
-        print("File name:", end=" ")
+        print("File name:", end=' ')
         try:
             self.filename = input()
         except KeyboardInterrupt:
@@ -95,18 +127,63 @@ class ProcessControlBlock:
             cleanup()
             exit()  # If Ctrl-D, just exit.
 
-    def __str__(self):
-        string = ("%s\t%s\t%s\t%s\t%s"
-                  % (str(self.pid).rjust(3),
-                     str(self.filename).rjust(8),
-                     str(self.memstart).rjust(8),
-                     str(self.rw).rjust(3),
-                     str(self.file_length).rjust(11)))
-        return string
+    def set_cylinder(self):
+        print("Enter cylinder:", end=' ')
+        try:
+            self.location = int(input())
+        except ValueError:
+            print("Error, try again.")
+            self.set_cylinder()
+        except KeyboardInterrupt:
+            print()
+            cleanup()
+            exit()
+        except EOFError:
+            print()
+            cleanup()
+            exit()  # If Ctrl-D, just exit.
 
+    def get_avgburst(self):
+        avgbursts = 0
+        if len(self.bursts) > 0:
+            for x in self.bursts:
+                avgbursts += x
+            avgbursts /= len(self.bursts)
+        return avgbursts
+
+    def print_ready(self):
+        string = ("%s\t%s\t%s\t%s"
+                  % (str(self.pid).rjust(3),
+                     str(self.memstart).rjust(8),
+                     str(self.cpu_total(3)),
+                     str(self.avg_burst).rjust(3)))
+        print(string)
+
+    def print_device(self):
+        string = ("%s\t%s\t%s\t%s\t%s\t%s\t%s"
+                  % (str(self.pid).rjust(3),
+                     str(self.filename).rjust(5),
+                     str(self.memstart).rjust(5),
+                     str(self.rw).rjust(3),
+                     str(self.file_length).rjust(5),
+                     str(self.cpu_total).rjust(3),
+                     str(self.avg_burst).rjust(3),))
+
+        print(string)
+
+    def print_disk(self):
+        string = ("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s"
+                  % (str(self.pid).rjust(3),
+                     str(self.filename).rjust(5),
+                     str(self.memstart).rjust(5),
+                     str(self.rw).rjust(3),
+                     str(self.file_length).rjust(5),
+                     str(self.cpu_total).rjust(3),
+                     str(self.avg_burst).rjust(3),
+                     str(self.location).rjust(3)))
+        print(string)
 
 def create_block(block):
-    block.set_file_name()
     block.set_file_length()
     block.set_memstart()
     block.set_memend()
