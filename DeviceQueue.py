49f9540478d408d5590d3563f2cddfa79cbce07b
Abdullah Khan <khanabdullahj@gmail.com>
2016-04-22 21:46:24 -0400

    FSCAN works?

--- DeviceQueue.py
+++ DeviceQueue.py
@@ -12,8 +12,6 @@ This is the generic device queue that will be inherited by the actual devices.
 
 
 class DeviceQueue:
-    # We need a banner at the top so we know what's going on.
-
     def __init__(self):
         self.q = deque()
         self.number = None
@@ -81,17 +79,8 @@ class DiskQueue(DeviceQueue):
         self.banner = (" " * 4).join(["PID", "MEM", "R/W", "Cylinder", "\tCPU Time", "# Bursts", "AVG"])
         #  We need a second queue for FSCAN.
         self.r = deque()
-        #  We need to know whether the queue is frozen or not.
-        #  We also need to know if this is the first request.
-        self.frozen = False #  By default, no.
-        self.first_request = True #  By default, yes.
-
-    def __bool__(self):
-        """Return whether both deques are empty."""
-        if not self.q and not self.r:
-            return True
-        else:
-            return False
+        #  Need read/write head at position 0.
+        self.head = 0
 
     def set_num_cylinders(self, num):
         print("Disk " + str(num) + ": Enter number of disk cylinders:", end=' ')
@@ -116,63 +105,50 @@ class DiskQueue(DeviceQueue):
 
     def print_device_queue(self):
         print(self.banner)
-        if not self.q:
+        if not self.q and not self.r:
             print("Nothing in device queue.")
-            return
+
         if self.q:
-            print("Seek:")
+            print("Currently seeking:")
             for block in self.q:
                 block.print_disk_queue()
-
-    def add(self, block):
-        """Runs FSCAN to put the PCBs in order."""
-        if not self.frozen:
-            self.q.append(block)  # Add the PCB
-            self.fscan_sort()     # Sort the queue
-            self.on_request()     # Freeze the queue
-            return True
-        else:
-            self.r.append(block)
-            self.fscan_sort()
-            return True
-
-    def pop(self):
-        """Pop item from the front of DeviceQueue, and return it."""
-        if self.q:
-            if not self.q:
-                self.fscan_sort()
-            return self.q.popleft()
-        else:
-            return self.pop()
-
-    def freeze(self):
-        """Sets freeze to the opposite of whatever it was,
-           so we know to use another queue to collect requests."""
-        if(not self.frozen and not self.r) or (self.frozen and not self.q):
-            self.frozen = not self.frozen
-
-    def on_request(self):
-        """When we receive a request and both deques are empty, lock one queue and have the other take requests."""
-        if self.first_request:
-            self.freeze()
-            self.first_request = False
-        if not self.first_request and self:
-            self.frozen = True
-            self.first_request = True
+        if self.r:
+            print("Requests:")
+            for block in self.r:
+                block.print_disk_queue()
 
     def fscan_sort(self):
-        """Sort by PID first, then sort by cylinder."""
-        if not self:
-            return False
-        if len(self.q) > 1:
-            self.q = deque(sorted(self.q, key=lambda pcb: pcb.pid))
-            self.q = deque(sorted(self.q, key=lambda pcb: pcb.get_cylinder))
-        if len(self.r) > 1:
-            self.r = deque(sorted(self.r, key=lambda pcb: pcb.pid))
-            self.r = deque(sorted(self.r, key=lambda pcb: pcb.get_cylinder))
+        # Get a list of all requests ahead of where the head currently is.
+        forward = [idx for idx in self.r if self.r[-1].get_cylinder >= self.head]
+        # Now sort by PID, tie-breaker taken care of.
+        forward.sort(key=lambda pcb: pcb.pid)
+        # Sort by cylinder
+        forward.sort(key=lambda pcb: pcb.get_cylinder)
+        # Now we do the same for requests behind the head.
+        backward = [idx for idx in self.r if self.r[-1].get_cylinder < self.head]
+        backward.sort(key=lambda pcb: pcb.pid)
+        backward.sort(key=lambda pcb: pcb.get_cylinder, reverse=True)
+        # We can clear the requests queue now.
+        self.r.clear()
+        # Fill the primary queue with the sorted requests.
+        # deque takes iterables
+        self.q = deque(forward + backward)
 
 
+    def add(self, block):
+        self.r.append(block)
+        print("Success.")
+        if not self.q:
+            self.fscan_sort()
+            print("Queue empty, running fscan.")
 
+    def pop(self):
+        if self.q:
+            self.head = self.q[0].get_cylinder
+            finished = self.q.pop()
+            if not self.q:
+                self.fscan_sort()
+            return finished
 
 class PrinterQueue(DeviceQueue):
     def __init__(self):
